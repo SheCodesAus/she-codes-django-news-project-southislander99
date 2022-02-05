cddb60ec9b222402b0fa795a1d37b90ddf1e88b0
southislander99 <melwest000@gmail.com>
2022-02-05 12:53:28 +0800

    Created User Profile Page

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -8,3 +8,10 @@ class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+class UserProfileView(generic.DetailView):
+    model = CustomUser
+    template_name = 'users/userProfile.html'
+    context_object_name = 'userProfile'
+
+
